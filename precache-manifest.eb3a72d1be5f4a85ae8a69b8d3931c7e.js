bbc21a3d3af9381e820aa51faec805541942d109
indiraaridni <indiraaridni@mail.ru>
2020-06-11 16:40:47 -0400

    Updates

---
+++ precache-manifest.eb3a72d1be5f4a85ae8a69b8d3931c7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aade9ddeec3f5406047183be2fac8e5d",
+    "url": "/train_todolist_11jun/index.html"
+  },
+  {
+    "revision": "1398155129abaa3b0770",
+    "url": "/train_todolist_11jun/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "f4d723332057748bbe5e",
+    "url": "/train_todolist_11jun/static/css/main.e608b536.chunk.css"
+  },
+  {
+    "revision": "1398155129abaa3b0770",
+    "url": "/train_todolist_11jun/static/js/2.ddfa3841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/train_todolist_11jun/static/js/2.ddfa3841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d723332057748bbe5e",
+    "url": "/train_todolist_11jun/static/js/main.b94abcef.chunk.js"
+  },
+  {
+    "revision": "a4f60f194efd430b9a00",
+    "url": "/train_todolist_11jun/static/js/runtime-main.52b25867.js"
+  }
+]);
